fa78b54c1acbf058fdf8ab76b9600c4e8e56cd4f
kkukukun <kukukunyanyan@gmail.com>
2021-04-06 08:46:04 +0300

    Update Lab1.java
    2commit

--- src/lab1/Lab1.java
+++ src/lab1/Lab1.java
@@ -15,31 +15,36 @@ import org.hibernate.Session;
  *
  * @author 18738
  */
+
 public class Lab1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+
             ResultSet rs = null;
             Session session = NewHibernateUtil.getSessionFactory().openSession();
-            
+
             session.beginTransaction();
             String hql = "FROM Studentyi ";
             Query query = session.createQuery(hql);
-            
+
             List <Studentyi> studentyi = query.list();
             studentyi.size();
-            
+
             for (int i=0; i<=studentyi.size(); i++) {
                 System.out.println("\t" + studentyi.get(i).getFamiliya()  + "\t"
                         + studentyi.get(i).getImya() + "\t"
-                         + studentyi.get(i).getOtchestvo() + "\t" 
+                         + studentyi.get(i).getOtchestvo() + "\t"
                         + studentyi.get(i).getGruppyi().getNazvanie());
             }
-            
+
+            for(int i = 0; i<= studentyi.size();i++){
+              System.out.println("\t" + studentyi.get(i).getFamiliya());
+            }
+
 //               String hql1 = "select count(e) from Entity e";
-//            Query query1 = session.createQuery(hql1);    
-    }    
+//            Query query1 = session.createQuery(hql1);
+    }
 }
