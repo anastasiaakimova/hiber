b47394dffb1fc0ea9337f6358cec840835307140
Anastasiya Akimova <anastasiyaaki171@gmail.com>
2021-04-07 00:56:48 +0400

    idk

--- src/lab1/Lab1.java
+++ src/lab1/Lab1.java
@@ -2,17 +2,15 @@
 package lab1;
 
 import db.NewHibernateUtil;
+import entity.Gruppyi;
 import entity.Studentyi;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
+import java.util.List;
+
+
 /**
- *
  * @author 18738
  */
 public class Lab1 {
@@ -21,22 +19,37 @@ public class Lab1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
-            ResultSet rs = null;
-            Session session = NewHibernateUtil.getSessionFactory().openSession();
-            
-            session.beginTransaction();
-            String hql = "FROM Studentyi ";
-            Query query = session.createQuery(hql);
-            
-            List <Studentyi> studentyi = query.list();
-            studentyi.size();
-            
-            for (int i=0; i<=studentyi.size(); i++) {
-                System.out.println("\t" + studentyi.get(i).getFamiliya()  + "\t"
-                        + studentyi.get(i).getImya() + "\t"
-                         + studentyi.get(i).getOtchestvo() + "\t" 
-                        + studentyi.get(i).getGruppyi().getNazvanie());
-            }  
-    }    
+
+
+        Session session = NewHibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+        String hql = "FROM Studentyi ";
+        Query query = session.createQuery(hql);
+
+        List<Studentyi> studentyi = query.list();
+        studentyi.size();
+
+        for (int i = 0; i <= studentyi.size(); i++) {
+            System.out.println("\t" + studentyi.get(i).getFamiliya() + "\t"
+                    + studentyi.get(i).getImya() + "\t"
+                    + studentyi.get(i).getOtchestvo() + "\t"
+                    + studentyi.get(i).getGruppyi().getNazvanie());
+        }
+
+//Вывести сведения о кол-ве студентов, обучающихся по каждой специальности
+
+        List<Gruppyi> gruppyis = query.list();
+        gruppyis.size();
+
+        for (int i = 0; i <= gruppyis.size(); i++) {
+            System.out.println("\t" + gruppyis.get(i).getShifr() + "\t"
+                    + gruppyis.get(i).getNazvanie() + "\t"
+                    + studentyi.get()
+            );
+        }
+
+        session.getTransaction().commit();
+        NewHibernateUtil.shutdown();
+    }
 }
